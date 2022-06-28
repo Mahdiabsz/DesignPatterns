92a473051f174fd69fffbb85c7e522d097e73e35
mahdiabsz <mahdiabbaszadeh1376@gmail.com>
2022-06-28 12:32:54 +0430

    added singleton pattern

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,8 +1,19 @@
-﻿using DesignPatterns.Decorator;
+﻿using DesignPatterns;
+using DesignPatterns.Decorator;
 
+#region Decorator pattern
 IPizza pizza = new Pizza();
 IPizza cheesePizza = new CheeseDecorator(pizza);
 IPizza onionPizza = new OnionDecorator(cheesePizza);
 IPizza tomatoPizza = new TomatoDecorator(onionPizza);
 Console.WriteLine(tomatoPizza.GetPizza());
+Console.Write("Press enter to process singleton pattern : ");
 Console.ReadLine();
+#endregion
+
+#region Singleton pattern
+Singleton singleton = Singleton.Instance;
+Console.WriteLine(singleton.PrintText());
+Console.Write("Press enter to process singleton pattern : ");
+Console.ReadLine();
+#endregion
